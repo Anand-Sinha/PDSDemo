226518ff4276ed9238e3ba6a7261af3e267bdcd4
Anand Sinha <anandsinha138@gmail.com>
2022-06-26 11:31:15 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
 const goToTopBtn = document.getElementById('goToTop')
 const navbar = document.getElementById('navbar')
+const ammunitionItem = document.getElementById('ammunitionItem')
+const ammunitionItemHeader = document.getElementById('ammunitionItemHeader')
+const componentsItem = document.getElementById('componentsItem')
+const componentsItemHidden = document.getElementById('componentsItemHidden')
+
+ammunitionItem.onmouseover = function(){
+  document.getElementById('ammunitionItemHidden').style.display = "block";
+};
+
+componentsItem.onmouseover = function(){
+  document.getElementById('componentsItemHidden').style.display = "block";
+};
+
+ammunitionItemHeader.onmouseleave = function(){
+
+  document.getElementById('ammunitionItemHidden').style.display = "none";
+  document.getElementById('componentsItemHidden').style.display = "none";
+};
 
 window.addEventListener('scroll',()=>{
     if(window.scrollY==0){
-        goToTopBtn.classList.add('gototop__hide')
+        goToTopBtn.classList.add('gototop__hide');
       }
     else{
         goToTopBtn.classList.remove('gototop__hide')
